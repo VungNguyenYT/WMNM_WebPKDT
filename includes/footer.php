30c698b44c7a2a8b0e753a469f8072223d2d5c50
Nguyễn Văn Vửng <130837811+VungNguyenYT@users.noreply.github.com>
2024-12-17 15:03:04 +0700

    HEADER and FOOTER
    HEADER and FOOTER

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+</main>
+<footer>
+    <div class="container">
+        <p>&copy; <?= date('Y') ?> GH STORE - Chuyên phụ Kiện Điện Thoại. </p>
+        <p>Địa chỉ: Số 126 Nguyễn Thiện Thành, khóm 4, phường 5 , Thành phố Trà Vinh , Trà Vinh , Việt Nam | Điện thoại: 0347482012</p> <br>
+        <strong style="font-size: large;"> SẢN PHẨM DEMO PHỤC VỤ ĐỒ ÁN KẾT THÚC MÔN PHÁT TRIỂN ỨNG DỤNG WEB MÃ NGUỒN MỞ</strong>
+        <p> <strong>Tên đề tài:</strong> Xây dựng website bán phụ kiện điện thoại</p>
+        <p><strong>Thành viên thực hiện:</strong> Nguyễn Văn Vửng - Đinh Lê Bảo Duy - Lý Mỹ Duyên</p>
+        <p><strong>Lớp:</strong> DA21TTA</p>
+    </div>
+</footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GH Store- Chuyên Phụ Kiện Điện Thoại</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href="index.php"> GH STORE </a>
+            </div>
+            
+            <!-- THANH TÌM KIẾM -->
+            <div class="search-bar" style="display: flex;" >
+                <form style="display: flex; height: auto;" action="search_results.php" method="GET">
+                    <input style=" margin: auto ;" type="text" name="query" placeholder="Tìm kiếm sản phẩm..." required>
+                    <button style="width:auto ; height: auto; margin: 10px" type="submit">Tìm kiếm</button>
+                </form>
+            </div>
+
+            <!-- MẤY CÁI NÚT CƠ BẢN -->
+            <nav>
+                <ul>
+                    <li><a href="index.php">Trang chủ</a></li>
+                    <li><a href="cart.php">Giỏ hàng</a></li>
+                    <li><a href="login.php">Đăng nhập</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
