11ebaed5433f62a4790618764681b7b416bdc46c
Zữn <130837811+VungNguyenYT@users.noreply.github.com>
2025-01-11 02:20:37 +0700

    Update HomeController.php

--- phone-accessories/app/Http/Controllers/HomeController.php
+++ phone-accessories/app/Http/Controllers/HomeController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
+
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // Lấy tất cả danh mục sản phẩm
-        $categories = Category::all();
-
-        // Truyền dữ liệu đến view `home`
-        return view('home', compact('categories'));
+        return view('home'); // Trả về view trang chủ
     }
 }
